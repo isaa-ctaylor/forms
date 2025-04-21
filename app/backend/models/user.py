8336288d48ad7c2f2ac5317cddd083a395b0dcf9
Isaac Taylor <isaac@isaactaylor.xyz>
2025-04-21 22:46:29 +0100

    feat(models): :sparkles: update users table to include user uuid and member id

--- app/backend/models/user.py
+++ app/backend/models/user.py
@@ -1,15 +1,20 @@
+import uuid
+
 from database.database import Base
 from sqlalchemy import Boolean, Column, Integer, String
-from sqlalchemy.orm import relationship
 
 
 class User(Base):
     __tablename__ = "users"
 
     id = Column(Integer, primary_key=True, index=True)
+    uuid = Column(
+        String(36), unique=True, index=True, default=lambda: str(uuid.uuid4())
+    )
     email = Column(String, unique=True, index=True)
     hashed_password = Column(String)
     is_active = Column(Boolean, default=True)
+    member_id = Column(String(4), unique=True, index=True)
 
-    # You can define relationships to other models here, for example:
-    # items = relationship("Item", back_populates="owner")
+    def __repr__(self):
+        return f"<User id={self.id}, uuid={self.uuid}, email='{self.email}', member_id='{self.member_id}'>"
