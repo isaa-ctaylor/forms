1cd379d5101f4f90fc22a8d42037f178a82309b2
Isaac Taylor <isaac@isaactaylor.xyz>
2025-04-21 22:47:04 +0100

    feat(users): :sparkles: update user routes to use UUID and enhance user retrieval methods

--- app/backend/routers/users.py
+++ app/backend/routers/users.py
@@ -1,3 +1,5 @@
+from uuid import UUID
+
 from crud import user as user_crud
 from deps import get_db
 from fastapi import APIRouter, Depends, HTTPException
@@ -21,30 +23,39 @@ def read_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
     return users
 
 
-@router.get("/{user_id}", response_model=user_schema.User)
-def read_user(user_id: int, db: Session = Depends(get_db)):
-    db_user = user_crud.get_user(db, user_id=user_id)
+@router.get("/{user_uuid}", response_model=user_schema.User)
+def read_user(user_uuid: UUID, db: Session = Depends(get_db)):
+    db_user = user_crud.get_user_by_uuid(db, uuid=str(user_uuid))
     if db_user is None:
         raise HTTPException(status_code=404, detail="User not found")
     return db_user
 
 
-@router.put("/{user_id}", response_model=user_schema.User)
-def update_user(
-    user_id: int, user: user_schema.UserCreate, db: Session = Depends(get_db)
-):
-    db_user = user_crud.get_user(db, user_id=user_id)
+@router.get("/member_id/{member_id}", response_model=user_schema.User)
+def read_user_by_member_id(member_id: str, db: Session = Depends(get_db)):
+    db_user = user_crud.get_user_by_member_id(db, member_id=member_id)
     if db_user is None:
         raise HTTPException(status_code=404, detail="User not found")
-    updated_user = user_crud.update_user(db=db, user_id=user_id, user_update=user)
+    return db_user
+
+
+@router.put("/{uuid}", response_model=user_schema.User)
+def update_user(
+    uuid: UUID, user: user_schema.UserCreate, db: Session = Depends(get_db)
+):
+    uuid = str(uuid)
+    db_user = user_crud.get_user_by_uuid(db, uuid=uuid)
+    if db_user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+    updated_user = user_crud.update_user(db=db, uuid=uuid, user_update=user)
     if updated_user is None:
         raise HTTPException(status_code=404, detail="User not found")
     return updated_user
 
 
-@router.delete("/{user_id}", response_model=dict)
-def delete_user(user_id: int, db: Session = Depends(get_db)):
-    if user_crud.delete_user(db, user_id=user_id):
+@router.delete("/{uuid}", response_model=dict)
+def delete_user(uuid: UUID, db: Session = Depends(get_db)):
+    if user_crud.delete_user(db, uuid=str(uuid)):
         return {"ok": True}
     else:
         raise HTTPException(status_code=404, detail="User not found")
