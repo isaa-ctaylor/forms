df5c8ef592b3a19a6755f3e0e7ee97787b4ed643
Isaac Taylor <isaac@isaactaylor.xyz>
2025-04-23 00:23:43 +0100

    feat(router): :sparkles: include auth router for authentication endpoints

--- app/backend/main.py
+++ app/backend/main.py
@@ -1,7 +1,7 @@
 # main.py
 from database import database
 from fastapi import FastAPI
-from routers import users
+from routers import auth, users
 from sqlalchemy.orm import Session
 
 database.Base.metadata.create_all(bind=database.engine)
@@ -9,3 +9,4 @@ database.Base.metadata.create_all(bind=database.engine)
 app = FastAPI()
 
 app.include_router(users.router, prefix="/users", tags=["users"])
+app.include_router(auth.router, prefix="/auth", tags=["auth"])
