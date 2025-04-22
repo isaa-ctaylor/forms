1f90e5f9d6657d60136967dee287766f5912453f
Isaac Taylor <isaac@isaactaylor.xyz>
2025-04-23 00:23:28 +0100

    feat(auth): :sparkles: add get_current_user function for retrieving authenticated user

--- app/backend/deps.py
+++ app/backend/deps.py
@@ -1,7 +1,14 @@
 from typing import Generator
 
-from database.database import SessionLocal
+from database.database import SessionLocal, get_db
+from fastapi import Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
+from jose import JWTError
+from models.user import User
 from sqlalchemy.orm import Session
+from utils.auth import verify_access_token
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="auth/login")  # Define the login endpoint
 
 
 def get_db() -> Generator[Session, None, None]:
@@ -10,3 +17,27 @@ def get_db() -> Generator[Session, None, None]:
         yield db
     finally:
         db.close()
+
+
+def get_current_user(
+    db: Session = Depends(get_db), token: str = Depends(oauth2_scheme)
+) -> User:
+    """Retrieve the currently authenticated user."""
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        payload = verify_access_token(token)
+        if payload is None:
+            raise credentials_exception
+        user_uuid: int = payload.get("sub")
+        if user_uuid is None:
+            raise credentials_exception
+        user = db.query(User).filter(User.uuid == user_uuid).first()
+        if user is None:
+            raise credentials_exception
+        return user
+    except JWTError:
+        raise credentials_exception
