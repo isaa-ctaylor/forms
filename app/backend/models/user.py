654516e0a6f45672ddf7579ffd812c30520ef112
Isaac Taylor <isaac@isaactaylor.xyz>
2025-04-23 00:22:24 +0100

    feat(user): :sparkles: add role field and update user representation with details

--- app/backend/models/user.py
+++ app/backend/models/user.py
@@ -1,7 +1,7 @@
 import uuid
 
 from database.database import Base
-from sqlalchemy import Boolean, Column, Integer, String
+from sqlalchemy import JSON, Boolean, Column, Integer, String, Enum
 
 
 class User(Base):
@@ -14,7 +14,13 @@ class User(Base):
     email = Column(String, unique=True, index=True)
     hashed_password = Column(String)
     is_active = Column(Boolean, default=True)
-    member_id = Column(String(4), unique=True, index=True)
+    details = Column(JSON, default={})
+    role = Column(
+        Enum("admin", "user", "moderator", name="user_roles"), default="user"
+    )  # Add role field
 
     def __repr__(self):
-        return f"<User id={self.id}, uuid={self.uuid}, email='{self.email}', member_id='{self.member_id}'>"
+        return (
+            f"<User id={self.id}, uuid={self.uuid}, email='{self.email}', "
+            f"details={self.details}, role={self.role}>"
+        )
