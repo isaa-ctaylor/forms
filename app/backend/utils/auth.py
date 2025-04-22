03863a28aa2d2f1e7277521374ca5766714aeba8
Isaac Taylor <isaac@isaactaylor.xyz>
2025-04-23 00:23:10 +0100

    feat(auth): :sparkles: add JWT token generation and verification functions

---
+++ app/backend/utils/auth.py
@@ -0,0 +1,41 @@
+import traceback
+from datetime import datetime, timedelta, timezone
+
+from config import settings
+from fastapi import HTTPException, status
+from jose import ExpiredSignatureError, jwt
+
+
+def create_access_token(data: dict) -> str:
+    """Generate a JWT token."""
+    to_encode = data.copy()
+
+    expire = datetime.now(timezone.utc) + timedelta(
+        minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES
+    )
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(
+        to_encode, settings.SECRET_KEY, algorithm=settings.ALGORITHM
+    )
+    return encoded_jwt
+
+
+def verify_access_token(token: str) -> dict:
+    """Verify a JWT token and return the payload."""
+    try:
+        payload = jwt.decode(
+            token, settings.SECRET_KEY, algorithms=[settings.ALGORITHM]
+        )
+        return payload
+    except ExpiredSignatureError:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Token has expired",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    except jwt.JWTError:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid token",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
